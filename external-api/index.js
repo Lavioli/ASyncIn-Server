fb28cdee0d1e4c6c74d1c1281e02bf6c867a02ab
LavieMaru <laviemaru@gmail.com>
2016-11-29 17:18:48 -0600

    create external-api folder for testing

---
+++ external-api/index.js
@@ -0,0 +1,54 @@
+require('babel-polyfill');
+import express from 'express';
+import bodyParser from 'body-parser';
+const jsonParser = bodyParser.json();
+import mongoose from 'mongoose';
+import passport from './config/passport';
+// const setCORS = require('./config/cors');
+
+mongoose.Promise = global.Promise;
+
+// const playlistsRouter = require('./routes/playlists');
+import usersRouter from './routes/users';
+import googleRouter from './config/google-oauth';
+const app = express();
+const User = require('./models/user');
+
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+// app.use('/api/v1/*', setCORS);
+// app.options("", (req, res) => res.sendStatus(200));
+app.post('*', jsonParser);
+app.put('*', jsonParser);
+app.use('/api/v1/users', usersRouter);
+app.use('/auth/google', googleRouter);
+// app.use('/api/v1/playlists', playlistsRouter);
+// app.use(passport.initialize());
+
+const CUSTOM_PORT = isNaN(Number(process.argv[2])) ? null : Number(process.argv[2]);
+
+const runServer = function (callback) {
+  const databaseUri = process.env.DATABASE_URI || global.databaseUri || 'mongodb://localhost/asyncin';
+  mongoose.connect(databaseUri).then(() => {
+    const port = process.env.PORT || 8080;
+    const server = app.listen(port, () => {
+      console.log(`Listening on port ${port}`);
+      if (callback) {
+        callback(server);
+      }
+    });
+  });
+};
+
+if (require.main === module) {
+  runServer(function () {
+    console.log("server started");
+  });
+}
+
+exports.app = app;
+exports.runServer = runServer;
