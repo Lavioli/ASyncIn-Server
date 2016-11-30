92507970751996ede35a05c6bc831b212f1fce74
LavieMaru <laviemaru@gmail.com>
2016-11-29 19:16:09 -0600

    youtubeRoute make request with top 5 result

---
+++ external-api/routes/client_secret.js
@@ -0,0 +1,7 @@
+const secret = {
+	youtube: {
+		api_key: 'AIzaSyCYoFzO03m75aa0cebl6fN5Gz1xPRovnJk'
+	}
+}
+
+export default secret;
--- external-api/routes/youtubeRoute.js
+++ external-api/routes/youtubeRoute.js
@@ -1,23 +1,20 @@
 const express = require('express');
 const youtubeRouter = express.Router();
-const Youtube = require('youtube-api');
 import request from 'request';
 import querystring from 'querystring';
-
-// import * youtubeAPI as from './auth';
+import secret from './client_secret';
 
 youtubeRouter
   .route('/')
   .post((req, res) => {
 
     const data = querystring.stringify({
-      key: 'AIzaSyCYoFzO03m75aa0cebl6fN5Gz1xPRovnJk',
+      key: secret.youtube.api_key,
       part: 'snippet',
       type: 'video',
       q: req.body.search
     });
 
-    // const q = req.body.query;
     request.get('https://www.googleapis.com/youtube/v3/search?' + data, (error, response, body) => {
       res.json(JSON.parse(response.body));
       }
