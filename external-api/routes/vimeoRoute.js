acc634070d0f0a26ab88f11d8b970024aed60662
LavieMaru <laviemaru@gmail.com>
2016-11-30 02:34:14 -0600

    add additional param options in comments, implement with selective response output vimeo

--- external-api/routes/vimeoRoute.js
+++ external-api/routes/vimeoRoute.js
@@ -16,9 +16,57 @@ vimeoRoute
 		const data = querystring.stringify({
 			access_token: access_token,
 			query: req.body.search,
+			per_page: 5,
+			page: 1
+
+		/*
+		ADDTIONAL PARAMS FOR CONSIDERATION:
+			filter: "string"
+				CC
+				CC-BY
+				CC-BY-SA
+				CC-BY-ND
+				CC-BY-NC
+				CC-BY-NC-SA
+				CC-BY-NC-ND
+				in-progress
+				upload_date
+				duration
+				minimum_likes
+				categories
+				trending
+			sort: "string"
+				relevant
+				date
+				alphabetical
+				plays
+				likes
+				comments
+				duration
+			direction: "string"
+				asc
+				desc
+			per_page: 100, //100 pp is max
+			page: 2, //(response.body).data shows page numbers from paging.last
+			uri: "/videos/31288701"  //uniform resource identifier
+
+		*/
+
 		});
 		request.get('https://api.vimeo.com/videos?' + data, (error, response, body) => {
-		  	  res.json(JSON.parse(response.body).data);
+			const resultArr = JSON.parse(response.body).data
+			const result = [];
+			resultArr.forEach((element) => {
+				result.push(
+					{
+						link: element.link,
+						name: element.name,
+						description: element.description,
+						pictures: element.pictures
+					}
+				);
+			});
+		  	res.json(result);
 		  	}
 		);
   	});
--- external-api/routes/youtubeRoute.js
+++ external-api/routes/youtubeRoute.js
@@ -14,10 +14,68 @@ youtubeRoute
       type: 'video',
       q: req.body.search,
       maxResults: 50
+      /*
+      ADDITIONAL PARAMS
+      order: string
+        date
+        rating
+        relevance
+        title
+        viewCount
+        videoCount
+      safeSearch: string
+        moderate
+        none
+        strict
+      videoCaption: string
+        any
+        closedCaption
+        none
+      videoDefinition
+        any
+        high
+        standard
+      videoDimension
+        2d
+        3d
+        any
+      videoDuration
+        any
+        long
+        medium   (4-20 min)
+        short    (4 min or less)
+      videoEmbeddable
+        any
+        true
+      videoLicense
+        any
+        creativeCommon
+        youtube
+      videoSyndicated
+        any
+        true
+      videoType
+        any
+        episode
+        movie
+      */
     });
 
     request.get('https://www.googleapis.com/youtube/v3/search?' + data, (error, response, body) => {
-      res.json(JSON.parse(response.body).items);
+      const resultsArr = JSON.parse(response.body).items;
+      const newResultsArr = [];
+      resultsArr.forEach((element) => {
+        newResultsArr.push(
+          {
+            videoId: element.id.videoId
+          },
+          {
+            snippet: element.snippet
+          }
+        )
+      })
+      res.json(newResultsArr);
+
       }
     )
   });
