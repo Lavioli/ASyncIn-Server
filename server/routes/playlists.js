2247bee9d17ac8a58e155bd471b5b292d3376ae4
KL012 <kevl927@gmail.com>
2016-12-01 23:53:28 +0000

    Correct response for routes

--- server/routes/playlists.js
+++ server/routes/playlists.js
@@ -10,3 +10,12 @@ const playlistsRouter = express.Router();
 
 playlistsRouter
   .route('/:userId')
+
+
+    // if(req.body.newPlaylists) {
+    //   User.findOneAndUpdate({ accessToken: req.query.access_token }, { playlists: req.body.newPlaylists }, { new:true })
+    //     .then(user => {
+    //       if (!user) return res.status(404).json({ message: 'User not found.' });
+    //         return res.json(user);
+    //     });
+    // }
