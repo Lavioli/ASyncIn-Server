b002371570cc0165c26d46e0022c835cf43291d1
KL012 <kevl927@gmail.com>
2016-12-02 07:43:33 +0000

    Organize and line up codes in users and playlists routes

--- server/routes/users.js
+++ server/routes/users.js
@@ -22,8 +22,8 @@ usersRouter
     if(!req.body.username || !req.body.password || !req.body.email) {
       return res.status(400).json({message: 'Invalid input.'});
     }
-    
-    User.createUser(
+    User.createUser
+    (
       req.body.username,
       req.body.password, 
       req.body.email,
@@ -31,12 +31,16 @@ usersRouter
     )
       .then(user => {
         res.set('Location', `/api/v1/users/${user.username}`);
-        return res.status(201).json({username: user.username, 
-                                     token: user.token, 
-                                     accessToken: user.accessToken, 
-                                     userId: user._id,
-                                     playlists: [],
-                                     favouritePlaylists: user.favouritePlaylists});
+        return res.status(201).json(
+          {
+            username: user.username, 
+            token: user.token, 
+            accessToken: user.accessToken, 
+            userId: user._id,
+            playlists: [],
+            favouritePlaylists: user.favouritePlaylists
+          }
+        );
       })
       .catch(err => {
         console.error(err);
@@ -50,46 +54,52 @@ usersRouter
   //If the user wants to change the password, current and new password will be sent to us in JSON format and accessToken via query
   //If the user wants to change the playlist send the updated playlist in JSON format and accessToken via query.
   .put(passport.authenticate('bearer', {session: false}), (req, res) => {
-    
     if(req.body.currentUsername && req.body.newUsername){
       if(req.body.currentUsername === req.body.newUsername) {
         return res.json({message: 'New username is same as the current username.'});
       }
     }
-    
     if(req.body.newUsername) {
-      User.findOneAndUpdate({accessToken: req.query.access_token },
-      { username: req.body.newUsername }, 
-      { new: true })
-       .then(user => {
-            if (!user) return res.status(404).json({message: 'User not found.'});
-            return res.json({username: user.username, 
-                             token: user.token, 
-                             accessToken: user.accessToken, 
-                             userId: user._id, 
-                             favouritePlaylists: user.favouritePlaylists});
-                           })
-          .catch(() => res.sendStatus(500));
+      User.findOneAndUpdate(
+        {accessToken: req.query.access_token},
+        { username: req.body.newUsername }, 
+        { new: true }
+      )
+      .then(user => {
+          if (!user) return res.status(404).json({message: 'User not found.'});
+          return res.json(
+            {
+              username: user.username, 
+              token: user.token, 
+              accessToken: user.accessToken, 
+              userId: user._id, 
+              favouritePlaylists: user.favouritePlaylists
+            }
+          );
+      })
+      .catch(() => res.sendStatus(500));
     }
-    
     if(req.body.newPassword) {
-       User.findOne({ accessToken: req.query.access_token },function(err, user){ 
-        bcrypt.genSalt(10, (err, salt) => {
-                      bcrypt.hash(req.body.newPassword, salt, (err, hashNewPassword) => {
-                        console.log('hashNewPassword',hashNewPassword);
-                      User.findOneAndUpdate({accessToken: req.query.access_token}, 
-                                            {password: hashNewPassword }, 
-                                            {new: true})
-                      .then(user => {
-                        if (!user) return res.status(404).json({ message: 'User not found.' });
-                          return res.json({message: 'Your password has been changed successfully.'});
-                      })
-                    .catch(() => res.sendStatus(500));
-            });
-        });
+      User.findOne({ accessToken: req.query.access_token },
+        function(err, user){ 
+          bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(req.body.newPassword, salt, 
+              (err, hashNewPassword) => {
+                User.findOneAndUpdate(
+                  {accessToken: req.query.access_token}, 
+                  {password: hashNewPassword }, 
+                  {new: true}
+                )
+                .then(user => {
+                  if (!user) return res.status(404).json({ message: 'User not found.' });
+                    return res.json({message: 'Your password has been changed successfully.'});
+                })
+                .catch(() => res.sendStatus(500));
+              }
+            );
+          });
       });
     }
-    
    if(!req.body.newUsername && !req.body.newPassword) {
       return res.status(404).json({ message: 'Invalid input' });
    }
@@ -104,22 +114,30 @@ usersRouter
       .then(user => {
         if (!user) return res.status(404).json({ message: 'User not found' });
         if (req.query.access_token === user.accessToken){
-           Playlist.find({userId: user._id}).then(playlist => {
-                return res.json({username: user.username, 
-                             token: user.token, 
-                             accessToken: user.accessToken, 
-                             userId: user._id,
-                             playlist: playlist,
-                             favouritePlaylists: user.favouritePlaylists});
-           });
+          Playlist.find({userId: user._id}).then(playlist => {
+            return res.json(
+              {
+                username: user.username, 
+                token: user.token, 
+                accessToken: user.accessToken, 
+                userId: user._id,
+                playlist: playlist,
+                favouritePlaylists: user.favouritePlaylists
+              }
+            );
+          });
         } else {
           //The else statement runs when an user checks out another user's playlist
-            Playlist.find({userId: user._id, isPublic: true}).then(playlist => {
-                return res.json({username: user.username,
-                             userId: user._id,
-                             playlist: playlist});
+            Playlist.find({userId: user._id, isPublic: true})
+            .then(playlist => {
+                return res.json(
+                  {
+                    username: user.username,
+                    userId: user._id,
+                    playlist: playlist
+                  }
+                );
             });
-
         }
       })
       .catch(err => res.sendStatus(500));
@@ -149,8 +167,6 @@ passport.use(
         }
     )
 );
-
-
 
 
 export default usersRouter;
