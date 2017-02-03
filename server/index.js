6bc2998301ffdee7e24fe39294744e23269b5bee
Lavie Ruan <lavieruan@Lavies-MacBook.local>
2017-02-03 02:48:43 -0600

    refactor code and remove unncessary codes

--- server/index.js
+++ server/index.js
@@ -1,36 +1,29 @@
 import 'babel-polyfill';
 import express from 'express';
 import bodyParser from 'body-parser';
-const jsonParser = bodyParser.json();
 import mongoose from 'mongoose';
 import passport from './config/passport';
 const setCORS = require('./config/cors');
-
-//this checks if the environement (env) is a developement environment and requires thee .env file. if it's not, it will take from heroku env config variables. 
-//probably will not need to require in index.js because we are not using the .env file here...
-if (process.env.NODE_ENV === 'development') {
-  require('dotenv').config();
-}
-
-mongoose.Promise = global.Promise;
-
-
 import usersRouter from './routes/users';
 import playlistsRouter from './routes/playlists';
 import googleRouter from './config/google-oauth';
 import facebookRouter from './config/facebook-oauth';
 import searchRouter from './routes/search-api';
 
+if (process.env.NODE_ENV === 'development') {
+  require('dotenv').config();
+}
+
+mongoose.Promise = global.Promise;
+
 const app = express();
 
 app.use(passport.initialize());
 app.use(passport.session());
-
+app.use('*', bodyParser.json());
 app.use('/api/v1/*', setCORS);
 app.use('/api/*', setCORS);
 app.options("", (req, res) => res.sendStatus(200));
-app.post('*', jsonParser);
-app.put('*', jsonParser);
 app.use('/api/v1/users', usersRouter);
 app.use('/api/v1/playlists', playlistsRouter);
 app.use('/auth/google', googleRouter);
@@ -57,6 +50,3 @@ if (require.main === module) {
     console.log("server started");
   });
 }
-
-exports.app = app;
-exports.runServer = runServer;
