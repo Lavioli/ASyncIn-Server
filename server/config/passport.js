605bb049270a96dc5c55587d076ae41d2354cd16
Lavie Ruan <lavieruan@Lavies-MacBook.local>
2017-02-03 03:00:29 -0600

    refactor passport.js

--- server/config/passport.js
+++ server/config/passport.js
@@ -1,16 +1,14 @@
 import passport from 'passport';
-import {BasicStrategy} from 'passport-http';
-
+import { BasicStrategy } from 'passport-http';
 import User from '../models/user';
 
-passport.use(new BasicStrategy((token, password, cb) => {
+export default passport.use(new BasicStrategy((token, password, cb) => {
   return User.findOneAndValidate(token, password)
     .then(user => {
-      if (!user) return cb(null, false);
 
-      return cb(null, user);
+     	if (!user) return cb(null, false);
+
+      	return cb(null, user);
     })
-
     .catch(err => cb(err));
 }));
-export default passport;
